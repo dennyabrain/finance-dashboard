336b3f9b6d603d4fbeaca49d75ed39e8a2be9c21
dennyabrain <denny.george90@gmail.com>
2022-07-09 11:54:10 +0530

    feat: add hashtag search

--- backend/features/search/db.js
+++ backend/features/search/db.js
@@ -1,6 +1,6 @@
 const { sequelize } = require("../../db/sequelize/models");
 
-async function search(searchTerm, bankFilters, page = 0) {
+async function searchText(searchTerm, bankFilters, page = 0) {
   bankFilterQuery =
     bankFilters.length === 0
       ? ""
@@ -21,6 +21,39 @@ async function search(searchTerm, bankFilters, page = 0) {
     `;
   const [tweets, metadata] = await sequelize.query(query);
   return { tweets };
+}
+
+async function searchHashtag(searchTerm, bankFilters, page = 0) {
+  bankFilterQuery =
+    bankFilters.length === 0
+      ? ""
+      : `AND EntityMentions.username IN ("${bankFilters.join('","')}")`;
+  const query = `
+    SELECT
+    MentionedTweets.id, MentionedTweets.text, MentionedTweets.eTwitterCreatedAt
+    FROM EntityHashtags 
+    LEFT JOIN Entities 
+    ON Entities.id = EntityHashtags.entityId 
+    LEFT JOIN EntityMentions 
+    ON Entities.id = EntityMentions.entityId
+    LEFT JOIN MentionedTweets 
+    ON MentionedTweets.id = Entities.mentionedTweetId 
+    WHERE EntityHashtags.tag="${searchTerm}" ${bankFilterQuery}
+    LIMIT ${page * 10},10;
+    `;
+  const [tweets, metadata] = await sequelize.query(query);
+  return { tweets };
+}
+
+async function search(searchTerm, ...args) {
+  let searchFunc;
+  if (searchTerm.startsWith("#")) {
+    let strippedSearchTerm = searchTerm.slice(1);
+    searchFunc = searchHashtag(strippedSearchTerm, ...args);
+  } else {
+    searchFunc = searchText(searchTerm, ...args);
+  }
+  return searchFunc;
 }
 
 module.exports = {
--- backend/features/search/search.test.js
+++ backend/features/search/search.test.js
@@ -1,6 +1,7 @@
 const { search } = require("./db");
 
 it("test search ", async () => {
-  const results = await search("password", ["canarabank", "pnbindia"], 0);
-  console.log(results);
+  // const results = await search("password", ["canarabank", "pnbindia"], 0);
+  const results = await search("#sandeepbakshi", [], 0);
+  expect(results.length).toBeGreaterThan(0);
 });
